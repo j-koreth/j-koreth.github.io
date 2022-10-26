4adba61217f78909f3f6b4a0c8fcdc5bc0d238ec
Joel Koreth <joelsymon@gmail.com>
2022-10-25 20:19:18 -0500

    Added photogrid and nextcloudcookbook

---
+++ _projects/nextcloud-cookbook.md
@@ -0,0 +1,45 @@
+---
+title: Nextcloud Cookbook
+date: 2022-10-10 00:00:00 Z
+name: Nextcloud Cookbook
+summary: Nextcloud Cookbook Mobile Client written in Flutter
+mytags: 
+- iOS
+- Flutter 
+- Dart 
+github: https://github.com/Teifun2/nextcloud-cookbook-flutter
+cover: assets/images/nextcloud-cookbook/2732x2048bb.png
+--- 
+
+<a href="https://apps.apple.com/us/app/nextcloud-cookbook/id1619926634?itsct=apps_box_badge&amp;itscg=30200" style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1665360000&h=87d711018a9b4c4324f6b4d0f036790f" alt="Download on the App Store" style="border-radius: 13px; width: 250px; height: 83px;"></a>
+
+
+<center>
+  <img src="https://tools-qr-production.s3.amazonaws.com/output/apple-toolbox/256571b5eff9b2dbf2208746df27600b/23592ffb84186cf797783c6921281cdf.png">
+</center>
+
+Ported a Flutter app to iOS and published to the app store as well as contributed timer features.
+
+<script src="{{ 'assets/js/photogrid.js' | relative_url}}"></script>
+
+
+
+<div id="photogrid" class="grid-container">
+      <div class="grid-item actions">
+          <a id="open" class="clickable button" href="" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i> Open</a>
+          <div>
+              <button id="prev" class="clickable button" onclick="nextcloudcookbook.prevphoto()">Previous</button>
+              <span style="margin: 0px 50px;"><span id="page_num"></span> / <span id="page_count"></span></span>
+              <button id="next" class="clickable button" onclick="nextcloudcookbook.nextphoto()">Next</button>
+          </div>
+          
+          <a id="download" class="clickable button" href="" download><i class="fa fa-download" aria-hidden="true"></i> Download</a>
+      </div>
+      <div class="grid-item"> 
+        <img src="" style="max-width: 300px">
+      </div>
+  </div>
+
+  <script type="text/javascript">
+   const nextcloudcookbook = new PhotoGrid('photogrid', ['{{ 'assets/images/nextcloud-cookbook/1242x2688bb-2.png' | relative_url}}', '{{ 'assets/images/nextcloud-cookbook/1242x2688bb-3.png' | relative_url}}',  '{{ 'assets/images/nextcloud-cookbook/1242x2688bb-4.png' | relative_url}}',  '{{ 'assets/images/nextcloud-cookbook/1242x2688bb-5.png' | relative_url}}', '{{ 'assets/images/nextcloud-cookbook/1242x2688bb.png' | relative_url}}'])
+</script>
---
+++ assets/js/photogrid.js
@@ -0,0 +1,54 @@
+class PhotoGrid {
+    photoarray = [];
+    index = 0;
+    id = null;
+
+    constructor(id, p1){
+        this.id = id;
+        this.photoarray.push(p1);
+        this.photoarray = this.photoarray.flat();
+        let photogrid = document.getElementById(this.id);
+        let image = photogrid.querySelector('div.grid-item:nth-child(2) > img:nth-child(1)');
+        image.src = p1[0];
+        let open = photogrid.querySelector('#open');
+        open.href = p1[0];
+        let download = photogrid.querySelector('#download');
+        download.href = p1[0];
+
+        let pagenumber = photogrid.querySelector('#page_num')
+        pagenumber.innerHTML = this.index + 1;
+
+        let pagecount = photogrid.querySelector('#page_count')
+        pagecount.innerHTML = this.photoarray.length;
+    }
+
+    nextphoto(){
+        let photogrid = document.getElementById(this.id);
+        let image = photogrid.querySelector('div.grid-item:nth-child(2) > img:nth-child(1)');
+        let open = photogrid.querySelector('#open');
+        let download = photogrid.querySelector('#download');
+        
+        this.index++;
+        image.src = this.photoarray[(this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length];
+        open.href = this.photoarray[(this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length];
+        download.href = this.photoarray[(this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length];
+
+        let pagenumber = photogrid.querySelector('#page_num')
+        pagenumber.innerHTML = (this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length + 1;
+    }
+
+    prevphoto(){
+        let photogrid = document.getElementById(this.id);
+        let image = photogrid.querySelector('div.grid-item:nth-child(2) > img:nth-child(1)');
+        let open = photogrid.querySelector('div.grid-item:nth-child(1) > a.clickable:nth-child(1)');
+        let download = photogrid.querySelector('div.grid-item:nth-child(1) > a.clickable:nth-child(3)');
+
+        this.index--;
+        image.src = this.photoarray[(this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length];
+        open.href = this.photoarray[(this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length];
+        download.href = this.photoarray[(this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length];
+
+        let pagenumber = photogrid.querySelector('#page_num')
+        pagenumber.innerHTML = (this.index % this.photoarray.length + this.photoarray.length) % this.photoarray.length + 1;
+    }
+}
